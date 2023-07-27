ac24f47f650751719188e0d32da57b6c4f0b19fa
ngw051004 <140357953+ngw051004@users.noreply.github.com>
2023-07-27 10:20:16 +0900

    Add files via upload

---
+++ 과제234.py
@@ -0,0 +1,12 @@
+a=int(input('첫번째 수를 입력하세요:'))
+b=int(input('두번째 수를 입력하세요:'))
+
+# a,b=input('두개의 정수를 입력하세요:').split()
+# 10 20 a=10 b=20
+
+#print(a+b)
+
+a,b=map(int, input('두개의 정수를 입력하세요';).split(','))
+
+
+print(a+b)
